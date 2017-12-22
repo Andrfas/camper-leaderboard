e13633264c2ec9ab0a8efb8328ef01685e9263cf
Vitalii Kulyk <vitcool@ukr.net>
2017-12-22 18:19:23 +0200

    CL-1: work with api changed after mr

--- src/components/FilterRow.js
+++ src/components/FilterRow.js
@@ -4,7 +4,6 @@ import "../styles/FilterRow.css";
 import { constFilterType } from "../const/api";
 import PropTypes from "prop-types";
 
-
 class FilterRow extends Component {
   constructor(props) {
     super(props);
@@ -14,25 +13,19 @@ class FilterRow extends Component {
   }
   onFilterClick = type => {
     if (type !== this.state.type) {
-      if (typeof this.props.onFilterClick === "function") {
-        this.setState({ type: type });
-        this.props.onFilterClick(type);
-      }
+      this.setState({ type: type });
+      this.props.onFilterClick(type);
     }
   };
   render() {
-    const {type} = this.state;
+    const { type } = this.state;
     return (
       <tr className="filter-row-component">
         <th className="number">#</th>
         <th>Campname</th>
         <th className="filter-title" ref="recent">
           <span
-            className={
-              type === constFilterType.recent
-                ? "selected-filter"
-                : ""
-            }
+            className={type === constFilterType.recent ? "selected-filter" : ""}
             onClick={this.onFilterClick.bind(this, constFilterType.recent)}
           >
             Points in past 30 days
@@ -41,9 +34,7 @@ class FilterRow extends Component {
         <th className="filter-title" ref="allTime">
           <span
             className={
-              type === constFilterType.allTime
-                ? "selected-filter"
-                : ""
+              type === constFilterType.allTime ? "selected-filter" : ""
             }
             onClick={this.onFilterClick.bind(this, constFilterType.allTime)}
           >
@@ -56,7 +47,7 @@ class FilterRow extends Component {
 }
 
 FilterRow.propTypes = {
-  onFilterClick: PropTypes.func    
-}
+  onFilterClick: PropTypes.func
+};
 
 export default FilterRow;
